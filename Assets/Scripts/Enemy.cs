3edb372636f93fc1553087d934bf638ad279d581
FloppyDr <DimaAlexandrov@yandex.ru>
2020-10-27 19:06:04 +0300

    Init

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Transform _wayPoint;
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _wayPoint.position, _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Enemey;
+
+    [SerializeField] private Transform _spawn;
+
+    private Transform[] _spawners;
+    private int _curretSpawner;
+    public float _timer = 0;
+
+    private void Start()
+    {
+        _spawners = new Transform[_spawn.childCount];
+
+        for (int i = 0; i < _spawn.childCount; i++)
+        {
+            _spawners[i] = _spawn.GetChild(i);
+        }
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >=2)
+        {
+            Instantiate(Enemey, _spawners[_curretSpawner].position, Quaternion.identity);
+            _curretSpawner++;
+            _timer = 0;
+        }
+
+        if (_curretSpawner >= _spawners.Length)
+        {
+            _curretSpawner = 0;
+        }
+    }
+
+}
